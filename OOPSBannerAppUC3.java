04f98c3c96a17f3e97e31316fc08177b0ad03a5c
1234Sree0707 <sreerammpm@gmail.com>
2026-02-18 13:27:29 +0530

    renamed

--- OOPSBannerAppUC3.java
+++ OOPSBannerAppUC3.java
@@ -9,7 +9,7 @@
 *@author Developer
 *@version 3.0
 */
-class OOPSBannerApp{
+class OOPSBannerAppUC3{
 public static void main(String[] args){
 	System.out.println(String.join(" ","   ***    ","     ***    ","   ******    ","  *****  "));
     System.out.println(String.join(" "," **   **  ","   **   **  ","   **    **  "," **      "));
