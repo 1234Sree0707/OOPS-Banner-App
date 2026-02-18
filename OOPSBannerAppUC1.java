6c07c4ae1080d67a4b40df6b66a52a15a93241a5
Sreeram Venugopal <135961017+1234Sree0707@users.noreply.github.com>
2026-02-18 13:35:18 +0530

    Update OOPSBannerAppUC1.java

--- OOPSBannerAppUC1.java
+++ OOPSBannerAppUC1.java
@@ -1,24 +1,14 @@
 /**
-*OOPSBannerApp UC1 - OOPS Banner Display Applicatin (Use Case 3)
+*OOPSBannerApp UC1 - OOPS Banner Display Applicatin
 *
-*This class extends the functionality of Use Case 2 by improving the way the "OOPS" banner
-*is constructed and displayed. It focuses on enhancing code readability and efficieny
-*by utilizing the String.join() method to create each line of the banner. This approach 
-*overcomes the drawbacks of using the + operator for string concatenation, which can lead
-*to code inefficiency due to the creation of multiple intermediate String objects in memory
+*This class demonstrates a simple Java application that displays the Object
+*oriented Programming System OOPS acronym to the console.
+*
 *@author Developer
-*@version 3.0
+*@version 1.0
 */
 class OOPSBannerAppUC1{
 public static void main(String[] args){
-	System.out.println(String.join(" ","   ***    ","     ***    ","   ******    ","  *****  "));
-    System.out.println(String.join(" "," **   **  ","   **   **  ","   **    **  "," **      "));
-    System.out.println(String.join(" ","**     ** ","  **     ** ","   **     ** "," **      "));
-    System.out.println(String.join(" ","**     ** ","  **     ** ","   **    **  "," **       "));
-    System.out.println(String.join(" ","**     ** ","  **     ** ","   ******    ","   ***    "));
-    System.out.println(String.join(" ","**     ** ","  **     ** ","   **        ","     **   "));
-    System.out.println(String.join(" ","**     ** ","  **     ** ","   **        ","      **  "));
-    System.out.println(String.join(" "," **   **  ","   **   **  ","   **        ","     **   "));
-    System.out.println(String.join(" ","   ***    ","     ***    ","   **        "," *****    "));
+System.out.println("OOPS");
 }
 }
