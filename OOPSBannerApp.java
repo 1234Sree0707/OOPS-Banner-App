3c44b148b2f1eb6c1a3a6645e3a6e4c2f5cee1e5
1234Sree0707 <sreerammpm@gmail.com>
2026-02-18 10:56:55 +0530

    Coded UC3 to print OOPS using String.join method

---
+++ OOPSBannerApp.java
@@ -0,0 +1,24 @@
+/**
+*OOPSBannerApp UC1 - OOPS Banner Display Applicatin (Use Case 3)
+*
+*This class extends the functionality of Use Case 2 by improving the way the "OOPS" banner
+*is constructed and displayed. It focuses on enhancing code readability and efficieny
+*by utilizing the String.join() method to create each line of the banner. This approach 
+*overcomes the drawbacks of using the + operator for string concatenation, which can lead
+*to code inefficiency due to the creation of multiple intermediate String objects in memory
+*@author Developer
+*@version 3.0
+*/
+class OOPSBannerApp{
+public static void main(String[] args){
+	System.out.println(String.join(" ","   ***    ","     ***    ","   ******    ","  *****  "));
+    System.out.println(String.join(" "," **   **  ","   **   **  ","   **    **  "," **      "));
+    System.out.println(String.join(" ","**     ** ","  **     ** ","   **     ** "," **      "));
+    System.out.println(String.join(" ","**     ** ","  **     ** ","   **    **  "," **       "));
+    System.out.println(String.join(" ","**     ** ","  **     ** ","   ******    ","   ***    "));
+    System.out.println(String.join(" ","**     ** ","  **     ** ","   **        ","     **   "));
+    System.out.println(String.join(" ","**     ** ","  **     ** ","   **        ","      **  "));
+    System.out.println(String.join(" "," **   **  ","   **   **  ","   **        ","     **   "));
+    System.out.println(String.join(" ","   ***    ","     ***    ","   **        "," *****    "));
+}
+}
